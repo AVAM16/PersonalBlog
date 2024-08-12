ab1d8d5cb5b25e112927852967adad94469376fe
AVAM16 <arturvasco.martins@gmail.com>
2024-08-12 15:53:18 +0100

    react and nextui

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Switch} from "@nextui-org/react";
+export default function App() {
+  return (
+    <Switch
+      defaultSelected
+      size="lg"
+      color="success"
+      //startContent={"A"}
+      //endContent={"B"}
+    >
+      Dark mode
+    </Switch>
+  );
+}
