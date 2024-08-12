3c4023a5b6201219a61fe468d7f391e53df2e6c9
AVAM16 <arturvasco.martins@gmail.com>
2024-08-12 19:14:48 +0100

    replaced nextui with daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,13 @@
-const { nextui } = require("@nextui-org/react");
-
-import { nextui } from '@nextui-org/react';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	darkMode: "class",
-	plugins: [nextui()],
+	plugins: [require('@tailwindcss/typography'),
+		require('daisyui'),
+	],
+	daisyui: {
+		themes: ["light", "dark"],
+	  },
 }
