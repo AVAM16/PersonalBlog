de54be9c46cc2030019928e942edf4298fbad5be
AVAM16 <arturvasco.martins@gmail.com>
2024-08-13 23:47:02 +0100

    navbar improvements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,24 @@
 import { Button } from "./ui/button";
-import { buttonVariants } from "@/components/ui/button"
 
+interface NavProps {
+  pageTitle: string;
+}
 
-export default function Nav() {
+export default function Nav({ pageTitle }: NavProps) {
   return (
     <div className="mr-4 hidden gap-2 md:flex">
-        <a href="/experience"><Button variant="ghost">Experience</Button></a>
-        <a href="/projects"><Button variant="ghost">Projects</Button></a>
-        <a href="/thenews"><Button variant="ghost">News</Button></a>
+        <Button asChild variant="secondary" className={`text-xl shadow-md dark:shadow-white shadow-black ${pageTitle === "Home" ? "text-blue-500" : ""}`}>
+          <a href="/">My Blog</a>
+        </Button>
+        <Button asChild variant="ghost" className={`${pageTitle === "Experience" ? "text-blue-500" : ""}`}>
+          <a href="/experience">Experience</a>
+        </Button>
+        <Button asChild variant="ghost" className={`${pageTitle === "My Projects" ? "text-blue-500" : ""}`}>
+          <a href="/projects">Projects</a>
+        </Button>
+        <Button asChild variant="ghost" className={`${pageTitle === "The News" ? "text-blue-500" : ""}`}>
+          <a href="/thenews">News</a>
+        </Button>
     </div>
   );
 }
