1fc0c744ea44fcea023ba6db5d41dbf92bb44dc1
AVAM16 <arturvasco.martins@gmail.com>
2024-08-14 17:35:01 +0100

    navbar done and footer

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger} from "./ui/sheet";
+import { Menu as MIcon} from "lucide-react";
 
-export default function Nav() {
+interface NavProps {
+  pageTitle: string;
+}
+
+export default function MobileNav({ pageTitle }: NavProps) {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="mr-4 hidden gap-2 md:flex">
-        <Button variant="ghost"></Button>
-    </div>
+    <Sheet open={open} onOpenChange={setOpen}>
+
+      <SheetTrigger asChild className="">
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <MIcon/>
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="top">
+          <div className="flex flex-col">
+            <Button asChild variant="link" onClick={() => { setOpen(false); }} className={`${pageTitle === "Experience" ? "text-blue-500" : ""}`}>
+              <a href="/experience">Experience</a>
+            </Button>
+            <Button asChild variant="link" onClick={() => { setOpen(false); }} className={`${pageTitle === "My Projects" ? "text-blue-500" : ""}`}>
+              <a href="/projects">Projects</a>
+            </Button>
+            <Button asChild variant="link" onClick={() => { setOpen(false); }} className={`${pageTitle === "The News" ? "text-blue-500" : ""}`}>
+              <a href="/thenews">News</a>
+            </Button>
+          </div>
+      </SheetContent>
+
+    </Sheet>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,9 +7,6 @@ interface NavProps {
 export default function Nav({ pageTitle }: NavProps) {
   return (
     <div className="mr-4 hidden gap-2 md:flex">
-        <Button asChild variant="secondary" className={`text-xl shadow-md dark:shadow-white shadow-black ${pageTitle === "Home" ? "text-blue-500" : ""}`}>
-          <a href="/">My Blog</a>
-        </Button>
         <Button asChild variant="ghost" className={`${pageTitle === "Experience" ? "text-blue-500" : ""}`}>
           <a href="/experience">Experience</a>
         </Button>
