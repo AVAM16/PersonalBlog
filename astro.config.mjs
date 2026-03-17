2e592ca1d94e0fa898dfd5050cf60711ec49f46f
Artur Martins <arturvasco.martins@gmail.com>
2026-03-17 10:58:31 +0000

    Major bump, Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({
+    tailwindcss({
       applyBaseStyles: false,
     }),
     react()
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
